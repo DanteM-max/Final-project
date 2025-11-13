2b9e1dd503231392a252dd8bc853beb7a0aa63a0
DanteM-max <DanteMatanza1301@gmail.com>
2025-11-13 23:02:29 +0000

    Almost done!

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ console.log("Script active");
 
 // Get all cells !!!-Credit to Copilot for teaching me querySelector and speeding up the coding process!-!!!
 let cells = document.querySelectorAll(".cell");
+for (let i = 0; i < cells.length; i++) {
+    cells[i].classList.add("vacant");
+}
 console.log(cells);
 
 function getCellsInColumn(column) {
@@ -21,6 +24,25 @@ for (let i = 0; i < 7; i++) {
 
 let dropButtons = document.querySelectorAll(".drop-button");
 for (let i = 0; i < dropButtons.length; i++) {
-    dropButtons[i].addEventListener("click", addDisc);
+    dropButtons[i].addEventListener("click", beginPlay);
 } 
 
+// Function to add a disc to the selected column
+function addDisc(column, playerNum) {
+    for (let i = getCellsInColumn(column).length - 1; i >=0; i--) {
+        //Thanks to Copilot for showing me the .classList.consains() method!
+        if (getCellsInColumn(column)[i].classList.contains("vacant")) {
+            if (playerNum == 1) {
+                getCellsInColumn(column)[i].classList.replace("vacant", "player1");
+            } else {
+                getCellsInColumn(column)[i].classList.replace("vacant", "player2");
+            }
+        }
+    }
+}
+
+function beginPlay(event) {
+    let column = event.target.id.charAt(event.target.id.length - 1);
+    addDisc(column, 1);
+    
+}
